e0aa2146fbf90dc87fbba47c19c2373b05dabbe7
kelvinOBA <obamakel1@gmail.com>
2024-06-27 11:19:31 +0100

    My portfolio in the works

---
+++ src/app/imageComponent.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+
+interface ImageComponentProps {
+    src: string | any;
+    alt: string;
+    description: string;
+}
+
+const ImageComponent: React.FC<ImageComponentProps> = ({
+    src,
+    alt,
+    description,
+}) => {
+    return (
+        <div className="imageContainer">
+            <div>
+                <Image
+                    width={60}
+                    height={60}
+                    src={src}
+                    alt={alt}
+                    className="image"
+                />
+            </div>
+
+            <div className="descriptionBox">
+                <div>
+                    <Image
+                        width={40}
+                        height={40}
+                        src={src}
+                        alt={alt}
+                        className="image"
+                    />
+                    {description} ncghgchc
+                    {description} ncghgchc
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageComponent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,145 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import user_img from "../../public/images/prof.png";
+import calc_ext from "../../public/images/calc.png";
+import ImageComponent from "./imageComponent";
+const parentContainer = [
+    {
+        name: "child1",
+        column1: "Hi!, I'm Kelvin a frontend Engineer, writer and teacher",
+
+        column2: "hello 3",
+    },
+    { name: "child2", column1: "content 1", column2: "content 2" },
+    { name: "child3", column1: "text 1", column2: "text 2" },
+];
+
+const images = [
+    {
+        src: "/images/autochek.jpeg",
+        alt: "Image 3",
+        description: "Description for Image 3Description for Image 3",
+    },
+    {
+        src: "/images/secapay.jpeg",
+        alt: "Image 1",
+        description: "Description for Image 1",
+    },
+    {
+        src: "/images/requid.png",
+        alt: "Image 2",
+        description: "Description for Image 2",
+    },
+    {
+        src: "/images/kidz.png",
+        alt: "Image 2",
+        description: "Description for Image 2",
+    },
+    {
+        src: "/images/cert.jpeg",
+        alt: "Image 2",
+        description: "Description for Image 2",
+    },
+];
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const updateChildContainer = (index: number) => {
+        setCurrentIndex(index);
+    };
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    const prevChild = () => {
+        const newIndex =
+            currentIndex > 0 ? currentIndex - 1 : parentContainer.length - 1;
+        updateChildContainer(newIndex);
+    };
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    const nextChild = () => {
+        const newIndex =
+            currentIndex < parentContainer.length - 1 ? currentIndex + 1 : 0;
+        updateChildContainer(newIndex);
+    };
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+    return (
+        <>
+            {/* <Image
+                src={user_img}
+                alt="great"
+                className="pattern"
+                width={2340}
+            /> */}
+            <div className={styles.parentContainer}>
+                <div className={styles.childContainer}>
+                    <div className={`${styles.column} ${styles.column1}`}>
+                        <Image
+                            src={user_img}
+                            alt="great"
+                            className="image-user"
+                            width={100}
+                        />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                        <h2 className="head-text">
+                            {parentContainer[currentIndex].column1}
+                        </h2>
+
+                        <p className="description mg-top-30">
+                            with Six Years of Experience Leading Frontend
+                            Projects at Early Stage and Series A Companies.
+                        </p>
+
+                        <div className="company-img">
+                            {images.map((image, index) => (
+                                <ImageComponent
+                                    key={index}
+                                    src={image.src}
+                                    alt={image.alt}
+                                    description={image.description}
+                                />
+                            ))}
+                        </div>
+
+                        <div>
+                            <a
+                                href="mailto:kelvin.igbinoba01.com"
+                                className="send-message"
+                            >
+                                Send a message
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className={`${styles.column} ${styles.column2}`}>
+                    <h2 >
+                          MY DEV/PROJECTS SHOWROOM
+                        </h2>
+
+<p className="mg-top-30">Calculator/ Unit Conversion chrome extension:</p>
+                        <Image
+                            src={calc_ext}
+                            alt="calculator extension"
+                            className="mg-top-30"
+                            // width={500}
+                            // height={300}
+                            layout="responsive"
+                        />
+                    </div>
+                </div>
+                <div
+                    className={`${styles.arrow} ${styles.arrowLeft}`}
+                    onClick={prevChild}
+                >
+                    &#8592;
+                </div>
+                <div
+                    className={`${styles.arrow} ${styles.arrowRight}`}
+                    onClick={nextChild}
+                >
+                    &#877a8b44;
+                </div>
+            </div>
+        </>
+    );
 }
