ae64d03d678e652756a5644adf4c225e7a8b7391
kelvinOBA <obamakel1@gmail.com>
2024-08-18 07:50:35 +0100

    Modify privacy policy

--- src/app/privacy-policy-discover-news/page.tsx
+++ src/app/privacy-policy-discover-news/page.tsx
@@ -11,7 +11,10 @@ const PrivacyPolicy = () => {
           This extension collects and stores the following data:
         </p>
         <ul>
-          <li><strong>Geolocation Data:</strong> We collect geolocation data, including your country code (optional), language, and region, solely for the purpose of providing you with localized news. This data is sent to our API and is not used for any other purposes.</li>
+          <li><strong>Language, timezone and or country code:
+            </strong> 
+            This extension collects few information which are language, country code (optional) and time zone. This data is stored locally and used to provide a more personalized experience (solely for the purpose of providing you with localized news). 
+            We may send this information to an external API to enhance functionality, but it is not used for any other purpose.</li>
           <li><strong>User Preferences:</strong>  Your preferences, such as theme settings, quick links, and other customization options, are stored locally using Chrome's storage system to enhance your browsing experience.</li>
         </ul>
       </div>
@@ -26,9 +29,8 @@ const PrivacyPolicy = () => {
       <div className="data-content">
         <h3>Permissions</h3>
         <ul>
-        <li><strong>Geolocation:</strong>  Used to determine your location for serving localized news.</li>
-        <li><strong>Storage:</strong> Used for saving user preferences, such as theme settings, quick links, and bookmarks.</li>
-        <li><strong>Bookmarks:</strong> Used to access and manage your bookmarks within the extension.</li>
+        <li><strong>Language, timezone and or country code :</strong>  Used to determine your location for serving localized news.</li>
+        <li><strong>Storage:</strong> Used for saving user preferences, such as theme settings, quick links.</li>
         </ul>
 
       </div>
@@ -45,6 +47,15 @@ const PrivacyPolicy = () => {
         <p>
         You have control over your data. You can manage your preferences and delete any stored data via the extension settings. Geolocation data is automatically removed when the extension is uninstalled.
         </p>
+      </div>
+      <div className="data-content">
+        <h3>        Third-Party Services
+        </h3>
+        <p><b>Google Fonts:</b></p>
+        <p>
+This extension uses to enhance the visual appearance. While no personal data is collected or stored by this extension, Google Fonts may log basic data such as IP addresses when serving the fonts.
+</p>
+
       </div>
 
 
